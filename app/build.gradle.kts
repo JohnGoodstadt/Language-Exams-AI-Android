68379c97126b876fbdc2fefc5f5a2100fd521578
John goodstadt <johngoodstadt@icloud.com>
2025-10-22 10:33:49 +0100

    V2.63

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ if (secretsFile.exists()) {
     secretsProperties.load(FileInputStream(secretsFile))
 }
 
-val VERSION_CODE = 62
-val VERSION_NAME = "1.62"
+val VERSION_CODE = 63
+val VERSION_NAME = "2.63"
 
 android {
     namespace = "com.goodstadt.john.language.exams" // Base namespace
