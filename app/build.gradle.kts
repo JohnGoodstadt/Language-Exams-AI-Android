7422eb21a37d7d0387a6577df21c0410cca12fd1
John goodstadt <johngoodstadt@icloud.com>
2025-11-25 10:03:08 +0000

    V71 2.71 Live in store

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ if (secretsFile.exists()) {
     secretsProperties.load(FileInputStream(secretsFile))
 }
 
-val VERSION_CODE = 70
-val VERSION_NAME = "2.70"
+val VERSION_CODE = 71
+val VERSION_NAME = "2.71"
 
 android {
     namespace = "com.goodstadt.john.language.exams" // Base namespace
