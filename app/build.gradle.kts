89cd782a508814bf9f8a7f6e3c1592ae86645f9d
John goodstadt <johngoodstadt@icloud.com>
2026-03-18 16:28:46 +0000

    V113

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ if (secretsFile.exists()) {
     secretsProperties.load(FileInputStream(secretsFile))
 }
 
-val VERSION_CODE = 112   //for remote config versioning.  localised quizzes
-val VERSION_NAME = "3.8.112" //go live now matches iOS 3.7
+val VERSION_CODE = 113   //for remote config versioning.  localised quizzes
+val VERSION_NAME = "3.8.113" //go live now matches iOS 3.7
 
 android {
     namespace = "com.goodstadt.john.language.exams" // Base namespace
